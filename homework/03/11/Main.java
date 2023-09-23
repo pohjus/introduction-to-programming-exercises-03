bb7acc3a641ddca70e734aa01d598b4d533abf71
pohjus <jussi.pohjolainen@gmail.com>
2023-09-23 15:41:37 +0300

    :sparkles: Add 03/11/Main.java

---
+++ homework/03/11/Main.java
@@ -0,0 +1,31 @@
+
+import static java.lang.System.out;
+
+public class Main {    
+    public static void main(String [] args) {
+        out.print("Give Number 1: ");
+        int number1 = Integer.parseInt(System.console().readLine());
+        out.print("Give operator (+, -, /, x): ");
+        String operator = System.console().readLine();
+        out.print("Give Number 2: ");
+        int number2 = Integer.parseInt(System.console().readLine());
+
+        double result = 0.0;
+
+        switch(operator) {
+            case "+":   result = number1 + number2; break;
+            case "-":   result = number1 - number2; break;
+            case "/":   if(number2 == 0) {
+                            out.println("do not divide with zero"); return; // end the app.
+                        } else {
+                            result = (double) number1 / number2; break;
+                        }                        
+            case "x":   result = number1 / number2; break;
+
+            default:   out.println("wrong operator"); return; // end the app.
+        }
+
+        out.println(result);
+    }
+
+}
