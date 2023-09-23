2300f4d99961b2b0cf9632551e77a769f595c304
pohjus <jussi.pohjolainen@gmail.com>
2023-09-23 14:53:15 +0300

    :sparkles: Add 03/09/Main.java

---
+++ homework/03/09/Main.java
@@ -0,0 +1,36 @@
+
+import static java.lang.System.out;
+
+public class Main {    
+    public static void main(String [] args) {
+        out.println("PALINDROME CHECKER");
+        out.print("Give word, I will check if it is a palindrome: ");
+        String originalWord = System.console().readLine();
+
+        String wordWithRemovedChars = "";
+
+        // let's remove chars
+        for(int i = 0; i < originalWord.length(); i++) {
+            if(originalWord.charAt(i) != ' ' && 
+               originalWord.charAt(i) != ',' && 
+               originalWord.charAt(i) != '.') {
+                wordWithRemovedChars += originalWord.charAt(i);
+            }
+        }
+        
+
+        boolean isPalindrome = true;
+
+        // let's turn everything to lowercase
+        wordWithRemovedChars = wordWithRemovedChars.toLowerCase();
+
+        for(int i = 0; i < wordWithRemovedChars.length(); i++) {
+            if(wordWithRemovedChars.charAt(i) != wordWithRemovedChars.charAt(wordWithRemovedChars.length() - 1 - i)) {
+                isPalindrome = false;
+                break; // ends the for loop
+            }
+        }
+
+        out.println(isPalindrome ? "It's palindrome!" : "It's not a palindrome");
+    }
+}
