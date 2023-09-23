d10e0d06ca2516c139a1a12f49829bc44d54dd41
pohjus <jussi.pohjolainen@gmail.com>
2023-09-23 17:56:42 +0300

    :bug: Fix 03/11/Main.java

--- homework/03/11/Main.java
+++ homework/03/11/Main.java
@@ -6,22 +6,22 @@ public class Main {
         out.print("Give Number 1: ");
         int number1 = Integer.parseInt(System.console().readLine());
         out.print("Give operator (+, -, /, x): ");
-        String operator = System.console().readLine();
+        char operator = System.console().readLine().charAt(0);
         out.print("Give Number 2: ");
         int number2 = Integer.parseInt(System.console().readLine());
+
+        if(number2 == 0 && operator == '/') {
+            out.println("do not divide with zero"); return; // end the app.
+        }
 
         double result = 0.0;
 
         switch(operator) {
-            case "+":   result = number1 + number2; break;
-            case "-":   result = number1 - number2; break;                       
-            case "x":   result = number1 / number2; break;
-            case "/":   if(number2 == 0) {
-                            out.println("do not divide with zero"); return; // end the app.
-                        } else {
-                            result = (double) number1 / number2; break;
-                        } 
-            default:   out.println("wrong operator"); return; // end the app.
+            case '+':   result = number1 + number2; break;
+            case '-':   result = number1 - number2; break;                       
+            case 'x':   result = number1 * number2; break;
+            case '/':   result = number1 / number2; break; 
+            default:    out.println("wrong operator"); return; // end the app.
         }
 
         out.println(result);
