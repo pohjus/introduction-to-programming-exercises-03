5b0ed64d897c5cc5809df9dd287cdb05b2c9706d
pohjus <jussi.pohjolainen@gmail.com>
2023-09-23 16:07:14 +0300

    :sparkles: Add 03/14/Main.java

---
+++ homework/03/14/Main.java
@@ -0,0 +1,49 @@
+
+import static java.lang.System.out;
+
+public class Main {    
+    public static void main(String [] args) {
+        out.print("Give pw: ");
+
+        // Returns an array full of characters
+        char [] characters = System.console().readPassword();
+
+        boolean isLengthOk = characters.length >= 8;
+        boolean isLowerCaseOk = false;
+        boolean isUpperCaseOk = false;
+        boolean isDigitOk = false;
+
+        for(int i = 0; i < characters.length; i++) {
+            if(Character.isLowerCase(characters[i])) {
+                isLowerCaseOk = true;
+            }
+            if(Character.isUpperCase(characters[i])) {
+                isUpperCaseOk = true;
+            }
+            if(Character.isDigit(characters[i])) {
+                isDigitOk = true;
+            }
+
+            // If all conditions are already ok, let's exit the loop.
+            if(isLowerCaseOk && isUpperCaseOk && isDigitOk) {
+                break;
+            }
+        }
+
+        if(isDigitOk && isLowerCaseOk && isLengthOk && isUpperCaseOk) {
+            out.println("Password ok.");
+            return;
+        }
+
+        String errorMsg = "Problems with pw:\n";
+
+        errorMsg += (!isLengthOk)    ? "   Must be 8 or more chars.\n" : "";
+        errorMsg += (!isLowerCaseOk) ? "   Must have lower case.\n" : "";
+        errorMsg += (!isUpperCaseOk) ? "   Must have upper case.\n" : "";
+        errorMsg += (!isDigitOk)     ? "   Must have digit.\n" : "";
+
+        out.println(errorMsg);
+             
+    }
+
+}
