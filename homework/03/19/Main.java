d51754e84ef1ab861fb8ce6d7e54db5ef22d1b11
pohjus <jussi.pohjolainen@gmail.com>
2023-09-23 16:34:09 +0300

    :sparkles: Add 03/19 Main.java

---
+++ homework/03/19/Main.java
@@ -0,0 +1,54 @@
+import java.io.Console;
+import static java.lang.System.out;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        int santeriX = 5, santeriY = 5;
+        int macX = 2, macY = 2;
+        
+        while (true) {
+            // Draw game
+            for (int y = 0; y < 10; y++) {
+                for (int x = 0; x < 10; x++) {
+                    if (x == santeriX && y == santeriY) {
+                        System.out.print("S");
+                    } else if (x == macX && y == macY) {
+                        System.out.print("M");
+                    } else if (x == 0 || x == 9 || y == 0 || y == 9) {
+                        System.out.print("#");
+                    } else {
+                        System.out.print("-");
+                    }
+                }
+                System.out.println();
+            }
+            
+            // Check victory
+            if (santeriX == macX && santeriY == macY) {
+                System.out.println("You won!");
+                break;
+            }
+            
+            // Santeri's skills
+            System.out.println("Liiku (W/A/S/D): ");
+            String move = System.console().readLine().toUpperCase();
+            
+            // Omitting { } and put everything on one line
+            if (move.equals("W") && santeriY > 1) santeriY--;
+            if (move.equals("A") && santeriX > 1) santeriX--;
+            if (move.equals("S") && santeriY < 8) santeriY++;
+            if (move.equals("D") && santeriX < 8) santeriX++;
+            
+            // Mac's skills
+            int macMove = (int) (Math.random() * 5);
+            
+            // Omitting { } and put everything on one line
+            if (macMove == 0 && macY > 1) macY--;
+            if (macMove == 1 && macX > 1) macX--;
+            if (macMove == 2 && macY < 8) macY++;
+            if (macMove == 3 && macX < 8) macX++;
+        }
+    }
+}
